27df4629fc6f8d83226032e78c4ceed7afd5cb6b
monikatamsoy <tamsoymonika@gmail.com>
2020-02-24 00:38:29 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends React.Component {
+  render(){
+  return (
+    <div className="App">
+      <header className="App-header">
+      
+        
+      </header>
+      <div className="card">
+  <div className="card-body">
+ hello
+  </div>
+</div>
+
+    </div>
+  );
+}
+}
+export default App;
