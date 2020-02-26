cba441fc26b9762fbb52c8bb8f326e754617e0e0
monikatamsoy <tamsoymonika@gmail.com>
2020-02-26 10:58:50 +0530

    add action function

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Plan from './components/Plan';
 
 class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      to : '',
+      from : '',
+      goingDate : '',
+      returnDate : '',
+      action : ''
+
+    }
+    this.setAction = this.setAction.bind(this);
+  }
+  setAction(e){
+    this.setState({action : e.target.value})
+    
+  };
+  debugger;
   render(){
+  
   return (
     <div className="App">
       <header className="App-header">
@@ -11,12 +30,20 @@ class App extends React.Component {
         
       </header>
       <div className="card">
-  <div className="card-body">
- hello
+  <div className="card-header">
+    
+  <button className="btn btn-link"  value="one-way" onClick = {this.setAction}>ONE WAY
+  {/* {this.state.action ? <Plan/> : console.log('hello')} */}
+  </button>
+  <button className="btn btn-link" value="round-trip" onClick = {this.setAction}>ROUND TRIP</button>
+  <button className="btn btn-link"  value="multi-city" onClick = {this.setAction}>MULTI CITY</button>
+  <button className="btn btn-link"  >PACKAGE</button>
   </div>
+    
 </div>
-
+  
     </div>
+   
   );
 }
 }
---
+++ src/components/Plan.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Plan extends React.Component{
+    render(){
+        return(
+            <div>
+                hello
+            </div>
+        )
+    }
+}
+export default Plan;
